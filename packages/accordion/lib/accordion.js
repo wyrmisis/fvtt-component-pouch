8492bacb194853b7075246a12a9ce3a26884c891
Tim Snyder <tim.snyder.work@gmail.com>
2021-09-07 13:28:37 -0400

    I liked the idea of importing the CSS vars for a component for easier reference, so we're doing it for the accordion too.

--- packages/accordion/lib/accordion.js
+++ packages/accordion/lib/accordion.js
@@ -1,5 +1,6 @@
 import {html, css, LitElement} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
+import styleVars from './styleVars.js';
 
 @customElement('pouch-accordion')
 export class Accordion extends LitElement {
@@ -45,57 +46,45 @@ export class AccordionItem extends LitElement {
   @property({type: String})
   title = 'Missing Title';
 
-  static styles = css`
-    :host {
-      --accordion-item-max-height: 300px;
+  static styles = [
+    styleVars,
+    css`
+      :host header {
+        font-family: var(--accordion-heading-font-family);
+        font-size: var(--accordion-heading-font-size);
+        color: var(--accordion-heading-font-color);
+        background: var(--accordion-heading-background);
+      }
 
-      --accordion-heading-background-color: #aaa;
-      --accordion-heading-font-family: "Signika", sans-serif;
-      --accordion-heading-font-color: #23221d;
-      --accordion-heading-font-size: 1.125em;
+      :host header:hover {
+        cursor: pointer;
+      }
 
-      --accordion-body-background-color: #ccc;
-      --accordion-body-font-family: "Signika", sans-serif;
-      --accordion-body-font-color: #191813;
-      --accordion-body-font-size: 1em;
-    }
+      :host header, 
+      :host([aria-expanded]) main {
+        padding: .5em .25em;
+      }
 
-    :host header {
-      font-family: var(--accordion-heading-font-family);
-      font-size: var(--accordion-heading-font-size);
-      color: var(--accordion-heading-font-color);
-      background: var(--accordion-heading-background-color);
-    }
+      :host main {
+        font-family: var(--accordion-body-font-family);
+        font-size: var(--accordion-body-font-size);
+        color: var(--accordion-body-font-color);
+        background: var(--accordion-body-background);
 
-    :host header:hover {
-      cursor: pointer;
-    }
+        max-height: 0;
+        transition: max-height .3s ease-in-out,
+                    margin .3s ease-in-out,
+                    padding .3s ease-in-out;
+        overflow: hidden;
+        margin: 0;
+        padding: 0 .25em;
+      }
 
-    :host header, 
-    :host([aria-expanded]) main {
-      padding: .5em .25em;
-    }
-
-    :host main {
-      font-family: var(--accordion-body-font-family);
-      font-size: var(--accordion-body-font-size);
-      color: var(--accordion-body-font-color);
-      background: var(--accordion-body-background-color);
-
-      max-height: 0;
-      transition: max-height .3s ease-in-out,
-                  margin .3s ease-in-out,
-                  padding .3s ease-in-out;
-      overflow: hidden;
-      margin: 0;
-      padding: 0 .25em;
-    }
-
-    :host([aria-expanded]) main {
-      max-height: var(--accordion-item-max-height);
-      margin: 0 .5em .5em;
-    }
-  `;
+      :host([aria-expanded]) main {
+        max-height: var(--accordion-item-max-height);
+        margin: 0 .5em .5em;
+      }
+    `];
 
   render() {
     return html`
---
+++ packages/accordion/lib/styleVars.js
@@ -0,0 +1,19 @@
+import { css } from 'lit';
+
+const styleVars = css`
+  :host {
+    --accordion-item-max-height: 300px;
+
+    --accordion-heading-background: #aaa;
+    --accordion-heading-font-family: "Signika", sans-serif;
+    --accordion-heading-font-color: #23221d;
+    --accordion-heading-font-size: 1.125em;
+
+    --accordion-body-background: #ccc;
+    --accordion-body-font-family: "Signika", sans-serif;
+    --accordion-body-font-color: #191813;
+    --accordion-body-font-size: 1em;
+  }
+`;
+
+export default styleVars;
