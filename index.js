2c7fc56cdb03fdd765888bab1b9e319becfb763e
Tim Snyder <tim.snyder.work@gmail.com>
2021-09-08 09:22:18 -0400

    Fix copy/paste issue

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import Accordion, { AccordionItem } from './packages/accordion/lib/accordion.js';
-import Typeahead from './packages/accordion/lib/accordion.js';
+import Typeahead from './packages/typeahead/lib/typeahead.js';
 
 console.info('Component Pouch | Registering components...')
 
