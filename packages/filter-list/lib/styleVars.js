61b76b0e8814268294656bfbbe0f20ca85b205e1
Tim <tim.snyder.work@gmail.com>
2021-09-09 15:31:27 -0400

    feat: Add a Filterable List component (#6)
    * Add the pouch-filter-list component
    
    * Add documentation

---
+++ packages/filter-list/lib/styleVars.js
@@ -0,0 +1,17 @@
+import { css } from 'lit';
+
+const styleVars = css`
+  :host {
+    --filter-list-input-font-family: "Signika", sans-serif;
+    --filter-list-input-font-size: 1em;
+    --filter-list-input-font-color: #191813;
+    --filter-list-input-background: #fff;
+
+    --filter-list-item-font-family: "Signika", sans-serif;
+    --filter-list-item-font-size: 1em;
+    --filter-list-item-font-color: #191813;
+    --filter-list-item-background: #ccc;
+  }
+`;
+
+export default styleVars;
